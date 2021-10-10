ca6f6523e0d65a74e742a61d4a3b966acc69d6d8
Soham Shinde <sohamshinde04@gmail.com>
2021-10-10 21:12:17 +0530

    added google authentication

--- server.js
+++ server.js
@@ -1,12 +1,194 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+const bodyParser = require("body-parser");
 const io = require('socket.io')(server)
+const ejs = require("ejs");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var session = require('express-session')
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
+var findOrCreate = require('mongoose-findorcreate')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session')
 
-app.use('/', express.static('public'))
+app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000, // in milliseconds
+  keys: ["cokiekey"]
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://soham:soham@cluster0.dfruy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+  console.log("Connected to mongoDB");
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  googleId: String,
+  emailId: String,
+  photo: String
+});
+
+const User = mongoose.model('user', userSchema);
+
+passport.serializeUser(function (User, done) {
+  done(null, User.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+
+  new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+    function (accessToken, refreshToken, profile, done) {
+      console.log('passport callback function fired:');
+      console.log(profile);
+      // check if the user already exists in our DB
+
+      User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          // already have the user
+          console.log("User already made");
+          console.log(currentUser);
+
+          done(null, currentUser)
+        } else {
+          // if not create the user in our db
+          console.log("Creating new user");
+          console.log(currentUser);
+          new User({
+            name: profile.displayName,
+            googleId: profile.id,
+            emailId: profile.emails[0].value,
+            photo: profile.photos[0].value
+          }).save().then((newUser) => {
+            console.log('new user created: ', newUser)
+          })
+
+          done(null, currentUser)
+        }
+      })
+    }
+  ));
+
+// home page -------------------------------------
+const authCheck = (req, res, next) => {
+  console.log("Currect user" + req.user);
+  if (req.user) {
+    res.redirect('/old');
+  } else {
+    next();
+  }
+};
+
+app.get('/', authCheck, (req, res) => {
+  res.render("home", {})
+});
+// old page -------------------------------------
+
+const authCheck2 = (req, res, next) => {
+  console.log("Currect user" + req.user);
+  if (req.user) {
+    next();
+    // res.redirect('/old');
+  } else {
+    res.redirect('/')
+    // next();
+  }
+};
+
+app.get('/old', authCheck2, (req, res) => {
+  console.log("HERE USER : ");
+  console.log(req.user);
+  console.log(typeof (req));
+  console.log(req.user.name);
+  res.render("index", { user: req.user.name, mail: req.user.emailId, image: req.user.photo })
+});
+
+//  login -----------------------------------
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email', 'openid'] }) // redirect to google singIn
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed_login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    console.log("Successful authentication of user : \n" + req.user);
+    console.log("Redirecting to /old");
+    res.redirect('/old');
+  });
+
+app.get('/failed_login', (req, res) => {
+  console.log(req);
+  res.send("login fail")
+})
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.send("LOGED OUT");
+});
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// socket ------------------------------------------
 io.on('connection', (socket) => {
   socket.on('join', (roomId) => {
     let selectedRoom = io.sockets.adapter.rooms[roomId]
