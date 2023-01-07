d158fb6e5b19dd3355f88b81d92489744dcae6b1
soham shinde <sohamshinde04@gmail.com>
2023-01-07 20:43:16 +0530

    seperated whitboard JS

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -20,11 +20,7 @@ let screenShareButton;
 let videoChanger;
 let tmpNewStream;
 
-// White Board vairable ----------------------------------
 
-var canvas = document.getElementsByClassName("whiteboard")[0];
-var colors = document.getElementsByClassName("color");
-var context = canvas.getContext("2d");
 
 // Configuration variables ===========================================
 
@@ -57,13 +53,6 @@ var peerConnectionConfig = {
 var recordRTCOptions = {
   mimeType: "video/webm",
 };
-
-// ! WHITEBOARD
-
-var current = {
-  color: "black",
-};
-var drawing = false;
 
 // ! START and Settup
 
@@ -246,7 +235,7 @@ function start() {
                 // tmpNewStream = new MediaStream()
                 // tmpNewStream.addTrack(stream.getVideoTracks()[0])
 
-                
+
                 videoChanger = peerConnections[peerUuid].pc.addTrack(track, localStream);
 
                 document.getElementById("localVideo").srcObject = tmpNewStream
@@ -285,29 +274,8 @@ function start() {
           });
         });
 
-        // ! WHITE BORAD START
-
-        for (var i = 0; i < colors.length; i++) {
-          colors[i].addEventListener("click", onColorUpdate, false);
-        }
-
         socket.on("drawing", onDrawingEvent);
 
-        window.addEventListener("resize", onResize, false);
-        onResize();
-
-        canvas.addEventListener("mousedown", onMouseDown, false);
-        canvas.addEventListener("mouseup", onMouseUp, false);
-        canvas.addEventListener("mouseout", onMouseUp, false);
-        canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
-
-        //Touch support for mobile devices
-        canvas.addEventListener("touchstart", onMouseDown, false);
-        canvas.addEventListener("touchend", onMouseUp, false);
-        canvas.addEventListener("touchcancel", onMouseUp, false);
-        canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
-
-        // ! WHITE BORAD END
 
         streams.push(canvas.captureStream());
         // recordingStream.srcObject = canvas.captureStream()
@@ -380,103 +348,6 @@ function appendMessage(owner, data) {
 }
 
 // ! END Messenger -------------------------------------------------------
-
-// ! START Whiteborad ---------------------------------------------------
-
-function drawLine(x0, y0, x1, y1, color, emit) {
-  context.beginPath();
-  context.moveTo(x0, y0);
-  context.lineTo(x1, y1);
-  context.strokeStyle = color;
-  context.lineWidth = 2;
-  context.stroke();
-  context.closePath();
-
-  if (!emit) {
-    return;
-  }
-  var w = canvas.width;
-  var h = canvas.height;
-
-  socket.emit("drawing", {
-    room: roomID,
-    data: {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color,
-    },
-  });
-}
-
-function onMouseDown(e) {
-  drawing = true;
-  current.x = e.clientX || e.touches[0].clientX;
-  current.y = e.clientY || e.touches[0].clientY;
-}
-
-function onMouseUp(e) {
-  if (!drawing) {
-    return;
-  }
-  drawing = false;
-  drawLine(
-    current.x,
-    current.y,
-    e.clientX || e.touches[0].clientX,
-    e.clientY || e.touches[0].clientY,
-    current.color,
-    true
-  );
-}
-
-function onMouseMove(e) {
-  if (!drawing) {
-    return;
-  }
-  drawLine(
-    current.x,
-    current.y,
-    e.clientX || e.touches[0].clientX,
-    e.clientY || e.touches[0].clientY,
-    current.color,
-    true
-  );
-  current.x = e.clientX || e.touches[0].clientX;
-  current.y = e.clientY || e.touches[0].clientY;
-}
-
-function onColorUpdate(e) {
-  current.color = e.target.className.split(" ")[1];
-}
-
-// limit the number of events per second
-function throttle(callback, delay) {
-  var previousCall = new Date().getTime();
-  return function () {
-    var time = new Date().getTime();
-
-    if (time - previousCall >= delay) {
-      previousCall = time;
-      callback.apply(null, arguments);
-    }
-  };
-}
-
-function onDrawingEvent(data) {
-  var w = canvas.width;
-  var h = canvas.height;
-  drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-}
-
-// make the canvas fill its parent
-function onResize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-// ! END Whiteboard ------------------------------------------------------------------
 
 // ! START WEBRTC Signalling ---------------------------------------------------------------
 
